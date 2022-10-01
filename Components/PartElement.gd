e227487e794afa7313be8e013bc9fdbdbff1da35
Liam <liamreadsspam@gmail.com>
2022-10-01 01:15:07 -0300

    Tween part health bars

--- Components/PartElement.gd
+++ Components/PartElement.gd
@@ -1,18 +1,26 @@
 extends VBoxContainer
 
 var part: Part
+var animated_health: float
 var orchestrator
 
 func init(part: Part, orchestrator):
 	self.part = part
 	self.orchestrator = orchestrator
 	self.name = part.name
+	self.animated_health = part.health
 	$Button.text = part.name
 	$ProgressBar.max_value = part.maximum_health
 
 func _process(delta):
 	if part != null:
-		$ProgressBar.value = part.health
+		if part.health != animated_health and !$Tween.is_active():
+			update_health()
+		$ProgressBar.value = animated_health
+
+func update_health():
+	$Tween.interpolate_property(self, "animated_health", animated_health, part.health, 0.3, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	$Tween.start()
 
 func toggle(value: bool):
 	$Button.disabled = !value
